ab9450254930c583df89728837ed084a8d0e2dce
praveen <100416844+PV27062001@users.noreply.github.com>
2023-12-06 20:13:26 +0530

    feature: added option for updaing employee

--- src/main/java/com/example/employeeapi/employee/EmployeeService.java
+++ src/main/java/com/example/employeeapi/employee/EmployeeService.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.NoSuchElementException;
 import java.util.Optional;
 
 @Service
@@ -26,6 +27,20 @@ public class EmployeeService {
     public String deleteEmployeeById(Long id){
          employeeRepository.deleteById(id);
          return "deleted";
+    }
+    public Optional<Employee> updateEmployee(Long id, Employee updatedEmployee) {
+        Optional<Employee> employeeOptional = employeeRepository.findById(id);
+
+        if (employeeOptional.isPresent()) {
+            Employee existingEmployee = employeeOptional.get();
+            existingEmployee.setName(updatedEmployee.getName());
+            existingEmployee.setDepartment(updatedEmployee.getDepartment());
+            existingEmployee.setPhoneNumber(updatedEmployee.getPhoneNumber());
+
+            return Optional.of(employeeRepository.save(existingEmployee));
+        } else {
+            throw new NoSuchElementException("No employee found with id: " + id);
+        }
     }
 
 
